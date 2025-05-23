ea889406d7ff8340fee54103747796b65ee2c1af
bparra26 <brayanpp2697@gmail.com>
2025-05-22 23:06:38 -0500

    feat: implemento metodos faltantes de mi repositorio de dominio

--- src/main/java/com/nequi/challenge/contexts/franchise/infrastructure/persistence/repositories/BranchOfficeRepository.java
+++ src/main/java/com/nequi/challenge/contexts/franchise/infrastructure/persistence/repositories/BranchOfficeRepository.java
@@ -5,10 +5,15 @@ import com.nequi.challenge.contexts.franchise.domain.repositories.IBranchOfficeR
 import com.nequi.challenge.contexts.franchise.infrastructure.mappers.BranchOfficeMapper;
 import com.nequi.challenge.contexts.franchise.infrastructure.persistence.collections.BranchOfficeDocument;
 import com.nequi.challenge.contexts.franchise.infrastructure.persistence.repositories.mongo.MongoBranchOfficeRepository;
+import com.nequi.challenge.contexts.shared.domain.constants.ErrorMessages;
+import com.nequi.challenge.contexts.shared.domain.exceptions.GenericNotFoundException;
+import com.nequi.challenge.contexts.shared.infrastructure.util.BuildErrorUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,16 +24,38 @@ public class BranchOfficeRepository implements IBranchOfficeRepository {
    @Override
    public Mono<BranchOffice> create(BranchOffice branchOffice) {
       BranchOfficeDocument document = this.mapper.toEntity(branchOffice);
+      document.setCreated_at(LocalDateTime.now());
       return this.repository.save(document).map(this.mapper::toDomain);
    }
 
    @Override
    public Mono<BranchOffice> update(String id, BranchOffice branchOffice) {
-      return null;
+      return findById(id)
+            .map(this.mapper::toEntity)
+            .map(document -> {
+               if (branchOffice.name() != null) document.setName(branchOffice.name());
+               if (branchOffice.franchiseId() != null) document.setFranchiseId(branchOffice.franchiseId());
+               document.setUpdated_at(LocalDateTime.now());
+               return document;
+            })
+            .flatMap(this.repository::save)
+            .map(this.mapper::toDomain);
+   }
+
+   public Mono<BranchOffice> findById(String id) {
+      return this.repository.findById(id).switchIfEmpty(
+                  Mono.error(new GenericNotFoundException(
+                        String.format("Branch office with id %s not found in the database", id),
+                        BuildErrorUtil.create(
+                              ErrorMessages.BRANCHOFFICE_NOT_FOUND_CODE,
+                              ErrorMessages.BRANCHOFFICE_NOT_FOUND_DESCRIPTION
+                        )))
+            )
+            .map(this.mapper::toDomain);
    }
 
    @Override
    public Flux<BranchOffice> findByFranchiseId(String franchiseId) {
-      return null;
+      return this.repository.findAllByFranchiseId(franchiseId).map(this.mapper::toDomain);
    }
 }
