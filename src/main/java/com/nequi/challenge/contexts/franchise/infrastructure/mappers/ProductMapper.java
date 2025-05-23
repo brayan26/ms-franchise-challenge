386418a818745d21eaf1fe5704b5901987b68f41
bparra26 <brayanpp2697@gmail.com>
2025-05-23 02:04:27 -0500

    feat: agrego clase mapper

---
+++ src/main/java/com/nequi/challenge/contexts/franchise/infrastructure/mappers/ProductMapper.java
@@ -0,0 +1,24 @@
+package com.nequi.challenge.contexts.franchise.infrastructure.mappers;
+
+import com.nequi.challenge.contexts.franchise.domain.model.Product;
+import com.nequi.challenge.contexts.franchise.infrastructure.adapters.dto.FranchiseRequestDto;
+import com.nequi.challenge.contexts.franchise.infrastructure.adapters.dto.ProductResponseDto;
+import com.nequi.challenge.contexts.franchise.infrastructure.persistence.collections.ProductDocument;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+   Product toDomain(FranchiseRequestDto dto);
+
+   ProductDocument toEntity(Product domain);
+
+   @Mapping(source = "id", target = "id")
+   @Mapping(source = "name", target = "name")
+   @Mapping(source = "created_at", target = "created_at")
+   @Mapping(source = "updated_at", target = "updated_at")
+   Product toDomain(ProductDocument document);
+
+   ProductResponseDto toResponseDto(Product domain);
+
+}
