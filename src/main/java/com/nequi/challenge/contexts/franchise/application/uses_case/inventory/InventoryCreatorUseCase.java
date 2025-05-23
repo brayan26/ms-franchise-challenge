11b42892dd8dcc79f4d5437a99e6b0b277ae6268
bparra26 <brayanpp2697@gmail.com>
2025-05-23 11:08:43 -0500

    feat: agrego implementacion de los casos de uso para el manejo de inventario

--- src/main/java/com/nequi/challenge/contexts/franchise/application/uses_case/inventory/InventoryCreatorUseCase.java
+++ src/main/java/com/nequi/challenge/contexts/franchise/application/uses_case/inventory/InventoryCreatorUseCase.java
@@ -1,4 +1,19 @@
 package com.nequi.challenge.contexts.franchise.application.uses_case.inventory;
 
-public class InvontoryCreatorUseCase {
+import com.nequi.challenge.contexts.franchise.domain.model.Inventory;
+import com.nequi.challenge.contexts.franchise.domain.repositories.IInventoryRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class InventoryCreatorUseCase {
+   private final IInventoryRepository repository;
+
+   public InventoryCreatorUseCase(IInventoryRepository repository) {
+      this.repository = repository;
+   }
+
+   public Mono<Inventory> execute(Inventory inventory) {
+      return this.repository.addProduct(inventory);
+   }
 }
--- src/main/java/com/nequi/challenge/contexts/franchise/application/uses_case/inventory/InventoryStockIUpdaterUseCase.java
+++ src/main/java/com/nequi/challenge/contexts/franchise/application/uses_case/inventory/InventoryStockIUpdaterUseCase.java
@@ -1,4 +1,19 @@
 package com.nequi.challenge.contexts.franchise.application.uses_case.inventory;
 
+import com.nequi.challenge.contexts.franchise.domain.model.Inventory;
+import com.nequi.challenge.contexts.franchise.domain.repositories.IInventoryRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
 public class InventoryStockIUpdaterUseCase {
+   private final IInventoryRepository repository;
+
+   public InventoryStockIUpdaterUseCase(IInventoryRepository repository) {
+      this.repository = repository;
+   }
+
+   public Mono<Void> execute(Inventory inventory) {
+      return this.repository.updateStock(inventory);
+   }
 }
