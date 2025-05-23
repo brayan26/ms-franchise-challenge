8824f58d76ffd07b005a2240f48b5fb7f8120d15
bparra26 <brayanpp2697@gmail.com>
2025-05-23 00:39:56 -0500

    feat: agrego implementacion de repositorio franquisia

---
+++ src/main/java/com/nequi/challenge/contexts/franchise/infrastructure/persistence/collections/FranchiseDocument.java
@@ -0,0 +1,22 @@
+package com.nequi.challenge.contexts.franchise.infrastructure.persistence.collections;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Document(collection = "franchise")
+public class FranchiseDocument {
+   @Id
+   private String id;
+   private String name;
+   private LocalDateTime created_at;
+   private LocalDateTime updated_at;
+}
