537c0e74518d0c9b38c3ae7069357462008351cb
bparra26 <brayanpp2697@gmail.com>
2025-05-23 14:17:58 -0500

    refactor: actualizo clase de dominio

--- src/main/java/com/nequi/challenge/contexts/franchise/domain/model/Product.java
+++ src/main/java/com/nequi/challenge/contexts/franchise/domain/model/Product.java
@@ -2,4 +2,4 @@ package com.nequi.challenge.contexts.franchise.domain.model;
 
 import java.time.LocalDateTime;
 
-public record Product(Long id, String name, LocalDateTime created_at, LocalDateTime updated_at) {}
+public record Product(String id, String name, LocalDateTime created_at, LocalDateTime updated_at) {}
