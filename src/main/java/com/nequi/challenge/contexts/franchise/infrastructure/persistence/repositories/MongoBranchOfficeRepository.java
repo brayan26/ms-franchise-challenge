625a31bf4aff41b289a4020f7c39193b350e793c
bparra26 <brayanpp2697@gmail.com>
2025-05-23 18:06:50 -0500

    fix: corrijo estructura de directorio en la capa de persistencia

--- src/main/java/com/nequi/challenge/contexts/franchise/infrastructure/persistence/repositories/MongoBranchOfficeRepository.java
+++ src/main/java/com/nequi/challenge/contexts/franchise/infrastructure/persistence/repositories/MongoBranchOfficeRepository.java
@@ -1,9 +1,11 @@
-package com.nequi.challenge.contexts.franchise.infrastructure.persistence.repositories.mongo;
+package com.nequi.challenge.contexts.franchise.infrastructure.persistence.repositories;
 
 import com.nequi.challenge.contexts.franchise.infrastructure.persistence.collections.BranchOfficeDocument;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface MongoBranchOfficeRepository extends ReactiveMongoRepository<BranchOfficeDocument, String> {
    Flux<BranchOfficeDocument> findAllByFranchiseId(String franchiseId);
 }
