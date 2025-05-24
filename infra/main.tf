9ac3bf2af88c7886020fb69d641828d173b4c135
bparra26 <brayanpp2697@gmail.com>
2025-05-24 01:23:56 -0500

    chore: actualizo manifiestos terraform para la creacion de un cluster k8s con autoscaling

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.46.0"
+      version = "2.54.0"
     }
   }
 }
@@ -16,11 +16,32 @@ resource "digitalocean_kubernetes_cluster" "nequi_cluster" {
   version = var.k8s_version
 
   node_pool {
-    name       = "default"
+    name       = "autoscale-pool-01"
     size       = var.node_size
     auto_scale = true
     min_nodes  = 1
     max_nodes  = 3
+    tags = ["backend"]
+
+    labels = {
+      service  = "backend"
+      priority = "high"
+    }
+  }
+}
+
+resource "digitalocean_kubernetes_node_pool" "autoscale-pool-01" {
+  cluster_id = digitalocean_kubernetes_cluster.nequi_cluster.id
+  name       = "autoscale-pool-01"
+  size       = var.node_size
+  auto_scale = true
+  min_nodes  = 1
+  max_nodes  = 3
+  tags       = ["backend"]
+
+  labels = {
+    service  = "backend"
+    priority = "high"
   }
 }
 
