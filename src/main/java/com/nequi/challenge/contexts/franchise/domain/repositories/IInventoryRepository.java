ff4232c8e37d136c3dd32b373aab4c685b272cbf
bparra26 <brayanpp2697@gmail.com>
2025-05-25 20:00:40 -0500

    fix: actualizo metodos para eliminar productos de inventario por sucursal y de manera transaccional, desde la coleccion de producto

--- src/main/java/com/nequi/challenge/contexts/franchise/domain/repositories/IInventoryRepository.java
+++ src/main/java/com/nequi/challenge/contexts/franchise/domain/repositories/IInventoryRepository.java
@@ -9,4 +9,6 @@ public interface IInventoryRepository {
    Mono<Void> updateStock(Inventory inventory);
    Flux<Inventory> getTopStockProductPerBranch(String franchiseId);
    Flux<Inventory> getInventoryByBranchOffice(String branchOfficeId);
+   Mono<Void> deleteInventoryByProductIdAndBranchOfficeId(String branchOfficeId, String productId);
+   Mono<Void> deleteProductWithInventory(String productId);
 }
